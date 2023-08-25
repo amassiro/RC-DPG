e44f247e2d2b28d7b5e7d0bb4a34ca3baf268cf0
amassiro <massironi.andrea@gmail.com>
2023-08-25 14:08:00 +0200

    up

--- Luminosity/loop.sh
+++ Luminosity/loop.sh
@@ -7,7 +7,9 @@
 #
 # for i in {7920..9075}
 # for i in {8273..9075}
-for i in {8178..9075}
+# for i in {8178..9075}
+# for i in {8894..9075}
+for i in {9059..9075}
 do
    echo "Fill $i"
    /cvmfs/cms-bril.cern.ch/brilconda3/bin/python3 prepareTree.py $i
